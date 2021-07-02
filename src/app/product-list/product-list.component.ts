8fd3d1491dd6c6e6d93d125ae53c184fed502f2b
ArbazSk <arbazshaikh5138@gmail.com>
2021-07-02 15:52:19 +0530

    used Lifecycle Hooks

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,13 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  
   pageTitle: string = "Product List";
-  products: any[] = [
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  listFilter: string = '';
+  products: IProduct[] = [
     {
       "productId": 1,
       "productName": "Leaf Rake",
@@ -27,7 +33,27 @@ export class ProductListComponent {
       "price": 32.99,
       "starRating": 4.2,
       "imageUrl": "assets/images/garden_cart.png"
+    },
+    {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2021",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "assets/images/hammer.png"
     }
   ];
+
+  toogleImage():void {
+    this.showImage = !this.showImage;
+  }
+
+  ngOnInit(): void{
+    console.log("In OnInit Method");
+  }
+
+
 
 }
