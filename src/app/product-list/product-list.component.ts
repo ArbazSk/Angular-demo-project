b89288fd4ab1b3265bcae2b767c71cef796b3d05
ArbazSk <arbazshaikh5138@gmail.com>
2021-07-02 16:39:47 +0530

    added filterBy feature

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -12,7 +12,11 @@ export class ProductListComponent implements OnInit {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  listFilter: string = '';
+  
+  private _listFilter: string = '';
+
+  filteredProducts: IProduct[] = [];
+
   products: IProduct[] = [
     {
       "productId": 1,
@@ -46,14 +50,32 @@ export class ProductListComponent implements OnInit {
     }
   ];
 
+  set listFilter(value: string){
+    this._listFilter = value;
+    console.log("In setter: ",value);
+    this.filteredProducts = this.performFilter(value);
+  }
+  get listFilter(): string{
+    return this._listFilter;
+  }
   toogleImage():void {
     this.showImage = !this.showImage;
   }
 
   ngOnInit(): void{
     console.log("In OnInit Method");
+    this._listFilter = 'cart';
+  }
+
+  performFilter(filterBy: string): IProduct[]{
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) => 
+    product.productName.toLocaleLowerCase().includes(filterBy));
+
   }
 
 
 
 }
+
+
